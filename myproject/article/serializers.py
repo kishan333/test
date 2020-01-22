38408e68c538658c28d9898509f30744b898500c
kishan333 <kishanpanchal123.kp@gmail.com>
2020-01-22 19:27:22 +0530

    myproject delete

--- myproject/article/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Article
-
-class ArticleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Article
-        fields = '__all__'
-        # print(dir(fileds))
-        # print(fields)
